c1b5c002a6fb5b2474ccb992e72b1155cd0ad913
qpingcode <hi@qping.me>
2021-02-21 11:05:48 +0800

    将dialect实现移动至databaseConnProp中 增加对DB2的支持

---
+++ src/main/java/me/qping/utils/database/connect/DataBaseDialect.java
@@ -0,0 +1,32 @@
+package me.qping.utils.database.connect;
+
+import me.qping.utils.database.connect.DataBaseConnectPropertes;
+import me.qping.utils.database.metadata.bean.FieldType;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @ClassName DataBaseDialect
+ * @Description 数据库方言
+ * @Author qping
+ * @Date 2019/12/12 16:28
+ * @Version 1.0
+ **/
+public interface DataBaseDialect {
+
+    public String getCatalogQuery();
+
+    public String getSchemaQuery();
+
+    /**
+     * 构建分页sql
+     * @param sql
+     * @param pageSize  每页数量
+     * @param pageNum   当前第几页，从0开始，当pageNum小于0时，会构建获取前pageSize条记录的sql
+     * @return
+     * @throws SQLException
+     */
+    public String getPageSql(String sql, int pageSize, int pageNum);
+
+}
